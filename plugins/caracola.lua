22f8d55adefe46c11a6d63ba338e5cc0f8dff24a
jarriztg <rickygamberroz@hotmail.com>
2016-05-16 05:23:03 -0500

    Update caracola.lua

--- plugins/caracola.lua
+++ plugins/caracola.lua
@@ -4,21 +4,24 @@ local triggers = {
 
 local action = function(msg, blocks, ln)
 	
-if blocks[1] == 'caracolamagica' then
-	math.randomseed(os.time());
-	end
-  num = math.random(0,3);
+if blocks[1] == 'caracolamagica' and blocks[2] then
+math.randomseed(os.time());
+end
+ num = math.random(0,3);
   if num == 0 then
-   api.sendPhoto(msg.chat.id, './caracola/si.jpg')
+	api.sendSticker(msg.chat.id,  './caracola/si.webp')
   elseif num == 1 then
-   api.sendPhoto(msg.chat.id, './caracola/no.jpg')
-  elseif num == 2 then
-   api.sendPhoto(msg.chat.id, './caracola/probablemente.jpg')
+    	api.sendSticker(msg.chat.id, './caracola/no.webp')
+ elseif num == 2 then
+	api.sendSticker(msg.chat.id,  './caracola/probablemente.webp')
   elseif num == 3 then
-   api.sendPhoto(msg.chat.id, './caracola/jamas.jpg')
+	api.sendSticker(msg.chat.id,  './caracola/nocreo.webp')
+
+   
  
     end
     end
+  
     
  return {
 	action = action,
