88539ca8525ef414da7e45d18b209e6ca143f674
jarriztg <rickygamberroz@hotmail.com>
2016-05-16 03:37:22 -0500

    actualizaciones y beta actualizada

--- bot.lua
+++ bot.lua
@@ -129,7 +129,7 @@ on_msg_receive = function(msg) -- The fn run whenever a message is received.
 	--Group language
 	msg.lang = db:get('lang:'..msg.chat.id)
 	if not msg.lang then
-		msg.lang = 'es'
+		msg.lang = 'en'
 	end
 	
 	collect_stats(msg) --resolve_username support, chat stats
