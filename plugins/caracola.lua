6ef441fbde7691d6f1e938564ef9500cba7dbbfe
jarriztg <rickygamberroz@hotmail.com>
2016-05-16 05:23:36 -0500

    actualizaciones y beta actualizada

--- plugins/caracola.lua
+++ plugins/caracola.lua
@@ -7,7 +7,7 @@ local action = function(msg, blocks, ln)
 if blocks[1] == 'caracolamagica' and blocks[2] then
 math.randomseed(os.time());
 end
- num = math.random(0,3);
+  num = math.random(0,3);
   if num == 0 then
 	api.sendSticker(msg.chat.id,  './caracola/si.webp')
   elseif num == 1 then
@@ -16,10 +16,7 @@ end
 	api.sendSticker(msg.chat.id,  './caracola/probablemente.webp')
   elseif num == 3 then
 	api.sendSticker(msg.chat.id,  './caracola/nocreo.webp')
-
-   
- 
-    end
+       end
     end
   
     
