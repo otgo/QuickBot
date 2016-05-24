62a54478d15aa7c4d74b93c42b39e8b46b38ef40
jarriztg <jarriztg@users.noreply.github.com>
2016-05-23 22:56:45 -0500

    anticrash

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 
 if [ "$1" = "install" ]; then
 sudo apt-get update && sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion && wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz && tar zxpf luarocks-2.2.2.tar.gz && cd luarocks-2.2.2 && sudo ./configure && sudo make bootstrap && sudo luarocks install luasocket && sudo luarocks install luasec && sudo luarocks install redis-lua && sudo luarocks install lua-term && sudo luarocks install serpent && sudo apt-get install curl && cd .. && sudo rm -Rf luarocks-2.2.2.tar.gz && sudo rm -Rf luarocks-2.2.2 
-    echo -e '\e[0;31mNOTA: al continuar eliminará el config.lua y lo volvera a hacer con los datos que ingreses\e[0m'
+    echo -e '\e[0;31mNOTA: al continuar eliminarÃ¡ el config.lua y lo volvera a hacer con los datos que ingreses\e[0m'
     echo -e '\e[0;31m      si ya haz modificado el config.lua, cierra el script. \e[0m'
-    echo "Te pedirá los datos necesarios para terminar de configurar el bot, por favor, ingresalos todos."
+    echo "Te pedirÃ¡ los datos necesarios para terminar de configurar el bot, por favor, ingresalos todos."
 read -n1 -r -p 'Presiona cualquier tecla para continuar'
 sudo ./run.sh config
 fi
@@ -104,29 +104,27 @@ if [ "$1" = "config" ]; then
     echo "}" >> config.lua
     clear
   echo -e '\e[0;32mOK. Configurado!\e[0m'
-  read -n1 -r -p 'A continuacion se abrirá el launcher del bot, cualquier falla, configuralo de nuevo.'
+  read -n1 -r -p 'A continuacion se abrirÃ¡ el launcher del bot, cualquier falla, configuralo de nuevo.'
   sudo ./run.sh
 fi
 
-if [ "$1" = "screen" ]; then
+if [ "$1" = "kp" ]; then
 
-sudo screen -X -S keepsession kill
-sudo service redis-server start
+	sudo screen -X -S running kill
+	sudo service redis-server start
   clear
-    while true; do
-      sudo screen -S keepsession -t screen lua bot.lua
-      sleep 4s
-    done
+	sudo screen -S running -t screen ./run.sh
+      
 fi
 
 
 if [ "$1" = "stop" ]; then
 
-sudo screen -X -S keepsession kill
+sudo screen -X -S running kill
   
 clear
 
-echo -e '\e[0;31mSession closed.\e[0m'
+echo -e '\e[0;31mBot detenido.\e[0m'
 
 fi
 
@@ -136,7 +134,7 @@ sudo rm -Rf logs/
   
 clear
 
-echo -e '\e[0;31mLogs deleted.\e[0m'
+echo -e '\e[0;31mLogs eliminados.\e[0m'
 
 fi
 
@@ -145,7 +143,23 @@ fi
 if [ "$1" = "" ]; then
 
 sudo service redis-server start
+sudo screen -X -S running kill
 clear
+
+while true
+do
+	lua bot.lua
+	echo -e '\e[0;31mCRASH DETECTADO\e[0m'
+	echo -e '\e[0;31mREINICIANDO\e[0m'
+	for i in 1
+do
+	echo "$i..."
+done
+	echo -e '\e[0;32m###########################################\e[0m'
+	echo -e '\e[0;32m#             Bot reiniciado              #"\e[0m'
+	echo -e '\e[0;32m###########################################"\e[0m'
+done
+
 
 while true; do
   lua bot.lua
