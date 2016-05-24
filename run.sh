ba07fe8dc18bc126b7731f0fdc161f63cd0750e3
jarriztg <jarriztg@users.noreply.github.com>
2016-05-23 23:00:02 -0500

    anticrash

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 
 if [ "$1" = "install" ]; then
-sudo apt-get update && sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion && wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz && tar zxpf luarocks-2.2.2.tar.gz && cd luarocks-2.2.2 && sudo ./configure && sudo make bootstrap && sudo luarocks install luasocket && sudo luarocks install luasec && sudo luarocks install redis-lua && sudo luarocks install lua-term && sudo luarocks install serpent && sudo apt-get install curl && cd .. && sudo rm -Rf luarocks-2.2.2.tar.gz && sudo rm -Rf luarocks-2.2.2 
+ sudo apt-get update && sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion && wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz && tar zxpf luarocks-2.2.2.tar.gz && cd luarocks-2.2.2 && sudo ./configure && sudo make bootstrap && sudo luarocks install luasocket && sudo luarocks install luasec && sudo luarocks install redis-lua && sudo luarocks install lua-term && sudo luarocks install serpent && sudo apt-get install curl && cd .. && sudo rm -Rf luarocks-2.2.2.tar.gz && sudo rm -Rf luarocks-2.2.2 
     echo -e '\e[0;31mNOTA: al continuar eliminará el config.lua y lo volvera a hacer con los datos que ingreses\e[0m'
     echo -e '\e[0;31m      si ya haz modificado el config.lua, cierra el script. \e[0m'
     echo "Te pedirá los datos necesarios para terminar de configurar el bot, por favor, ingresalos todos."
-read -n1 -r -p 'Presiona cualquier tecla para continuar'
-sudo ./run.sh config
+ read -n1 -r -p 'Presiona cualquier tecla para continuar'
+ sudo ./run.sh config
 fi
 
 if [ "$1" = "config" ]; then
