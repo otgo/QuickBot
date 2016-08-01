0d9cdd7a1b528605dcb472c6a94be094fa662330
Harry <jarriztg@users.noreply.github.com>
2016-08-01 12:43:48 -0500

    /gban 《ID》 agregado

--- plugins/gbanner.lua
+++ plugins/gbanner.lua
@@ -1,10 +1,16 @@
 local action = function(msg, matches)
 
-if msg.from.id ~= config.admin2 and msg.from.id ~= config.admin then
+if msg.from.id ~= config.admin and msg.from.id ~= config.admin2 then
 	return
 end
 
 if matches[1] == "gban" then
+ if matches[2] then
+  os.execute('echo "' ..matches[2].. '," >> ./data/gbans')
+		api.sendReply(msg, "ID " ..matches[2].. " *globalmente baneado*.", true)
+		bot_init(true)
+ end
+ if not matches[2] then
 	if msg.reply then
 		os.execute('echo "' ..msg.reply.from.id.. '," >> ./data/gbans')
 		api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globalmente baneado*.", true)
@@ -13,8 +19,15 @@ if matches[1] == "gban" then
 		api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
 	end
 end
+end
 
 if matches[1] == "ungban" then
+ if matches[2] then
+os.execute('sed -i "/' ..matches[2].. '/d" ./data/gbans')
+		api.sendReply(msg, "ID " ..matches[2].. " *globalmente desbaneado*.", true)
+		bot_init(true)
+end
+ if not matches[2] then
 	if msg.reply then
 		os.execute('sed -i "/' ..msg.reply.from.id.. '/d" ./data/gbans')
 		api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globalmente desbaneado*.", true)
@@ -22,6 +35,7 @@ if matches[1] == "ungban" then
 	else
 		api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
 	end
+ end
 end
 
 end
@@ -29,8 +43,10 @@ end
 return {
 	action = action,
 	triggers = {
-				'/(gban)',
-				'/(ungban)'
+				'^/(gban)$',
+				'^/(gban) (.*)$',
+				'^/(ungban)$',
+				'^/(ungban) (.*)$'
 				}
 		}
 
