b5f3e53852c1fc95f5b3506b28741dda7112947b
Harry <jarriztg@users.noreply.github.com>
2016-08-08 05:09:59 -0500

    función para banear/desbanear miembro con /gb/ungb

--- plugins/gbanner.lua
+++ plugins/gbanner.lua
@@ -5,39 +5,43 @@ if msg.from.id ~= config.admin and msg.from.id ~= config.admin2 then
 end
 
 if matches[1] == "gban" then
- if matches[2] then
-  os.execute('echo "' ..matches[2].. '," >> ./data/gbans')
+	
+	if matches[2] then
+  		os.execute('echo "' ..matches[2].. '," >> ./data/gbans')
 		api.sendReply(msg, "ID " ..matches[2].. " *globalmente baneado*.", true)
 		bot_init(true)
- end
- if not matches[2] then
-	if msg.reply then
-		os.execute('echo "' ..msg.reply.from.id.. '," >> ./data/gbans')
-		api.kickUser(msg.chat.id, msg.reply.from.id)
-		api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globalmente baneado*.", true)
-		bot_init(true)
-	else
-		api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
 	end
-end
+	
+ 	if not matches[2] then
+		if msg.reply then
+			os.execute('echo "' ..msg.reply.from.id.. '," >> ./data/gbans')
+			api.kickUser(msg.chat.id, msg.reply.from.id)
+			api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globalmente baneado*.", true)
+			bot_init(true)
+		else
+			api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
+		end
+	end
 end
 
 if matches[1] == "ungban" then
- if matches[2] then
-os.execute('sed -i "/' ..matches[2].. '/d" ./data/gbans')
+	
+	if matches[2] then
+		os.execute('sed -i "/' ..matches[2].. '/d" ./data/gbans')
 		api.sendReply(msg, "ID " ..matches[2].. " *globalmente desbaneado*.", true)
 		bot_init(true)
-end
- if not matches[2] then
-	if msg.reply then
-		os.execute('sed -i "/' ..msg.reply.from.id.. '/d" ./data/gbans')
-		api.unbanChatMember(msg.chat.id, msg.reply.from.id)
-		api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globalmente desbaneado*.", true)
-		bot_init(true)
-	else
-		api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
 	end
- end
+	
+ 	if not matches[2] then
+		if msg.reply then
+			os.execute('sed -i "/' ..msg.reply.from.id.. '/d" ./data/gbans')
+			api.unbanChatMember(msg.chat.id, msg.reply.from.id)
+			api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globalmente desbaneado*.", true)
+			bot_init(true)
+		else
+			api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
+		end
+	end
 end
 
 end
