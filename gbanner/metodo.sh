b8626a4e04e00fc04fd713fef874c266b1b13ff6
Harry <jarriztg@users.noreply.github.com>
2016-08-09 22:00:30 -0500

    Update metodo.sh

--- gbanner/metodo.sh
+++ gbanner/metodo.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-#*.*encoding=utf-8*.*
+#*.*encoding=utf-7*.*
 
 TOKEN=$(cat ./data/key)
 GBANS=$(cat ./data/gbans)
 
-URL='https://api.telegram.org/bot'$TOKEN
-
-ACCION=$URL'/kickChatMember'
-
-UPD_URL=$URL'/getUpdates?offset='
-OFFSET=0
+API='https://api.telegram.org/bot'$TOKEN
+ACCION=$API'/kickChatMember'
+getUpdates=$API'/getUpdates'
 
 banear_miembro() {
-	res=$(curl -s "$ACCION" -F "chat_id=$1" -F "user_id=$2")
+	cURL=$(curl -s "$ACCION" -F "chat_id=$1" -F "user_id=$2")
 }
 
 process_client() {
 
 	# User
-	user_id=$(echo "$res" | egrep '\["result",0,"message","from","id"\]' | cut -f 2)
-	chat_id=$(echo "$res" | egrep '\["result",0,"message","chat","id"\]' | cut -f 2)
-	new_chat_member=$(echo "$res" | egrep '\["result",0,"message","new_chat_member","id"\]' | cut -f 2 | cut -d '"' -f 2)
+	user_id=$(echo "$cURL" | egrep '\["result",0,"message","from","id"\]' | cut -f 2)
+	chat_id=$(echo "$cURL" | egrep '\["result",0,"message","chat","id"\]' | cut -f 2)
+	new_chat_member=$(echo "$cURL" | egrep '\["result",0,"message","new_chat_member","id"\]' | cut -f 2 | cut -d '"' -f 2)
 
 	# Read list of gbans
 	echo $GBANS | grep $user_id &>/dev/null
@@ -47,12 +44,11 @@ process_client() {
 
 while [ "$1" == "gbans" ]; do {
 
-	res=$(curl -s $UPD_URL | ./gbanner/decoder.sh -s)
+	cURL=$(curl -s $getUpdates | ./gbanner/decoder.sh -s)
 
 		if [ "$2" == "test" ]; then
  			process_client
  		else
  			process_client&
  		fi
-
 }; done
