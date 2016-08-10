0e868693bc2d8e12c922e2ac56d81f78ebf77d66
jarriztg <root@localhost>
2016-08-10 11:21:26 -0500

    hj

---
+++ plugins/bash.lua
@@ -0,0 +1,24 @@
+local function bash(msg, blocks)
+		local action = io.popen(blocks[1])
+		local read = action:read("*a")
+	return read
+end
+
+
+local action = function(msg, blocks)
+local command = "_"..bash(msg, blocks).."_"
+
+if msg.from.id ~= config.admin then
+   return
+end
+
+   api.sendMessage(msg.chat.id, command, true)
+   
+end
+
+return {
+        action = action,
+        triggers = {
+               '^/bash (.*)$'
+               }
+        }
