f406781b69d9a518c912cfeda235eef6c78f891b
Harry <jarriztg@users.noreply.github.com>
2016-08-15 23:38:30 -0500

    sessión segura agregada
    Lee readme

--- run.sh
+++ run.sh
@@ -1,9 +1,25 @@
 session=QuickBot
 session2=ScriptGban
+process=process-115-no_exit
+
+do_secure_process() {
+	secure="process-115"
+	while true
+	do
+		lua bot.lua
+		echo -e 'crash'
+	for i in 1
+	do
+		echo "$i..."
+	done
+		echo "terminado"
+	done
+}
 
 if [ "$1" = "" ]; then
 	clear
 	tmux kill-session -t $session
+	tmux kill-session -t $process
 	clear
 	tmux new-session -s "$session" -d 'lua bot.lua'
 	tmux attach -t "$session"
@@ -23,8 +39,20 @@ fi
 
 if [ "$1" = "kill" ]; then
 	clear
+	tmux kill-session -t $process
 	tmux kill-session -t $session
 	tmux kill-session -t $session2
 fi
 
+if [ "$1" == "$process" ]; then
+	do_secure_process
+fi
 
+if [ "$1" == "secure" ]; then
+	clear
+	tmux kill-session -t $session
+	tmux kill-session -t $process
+	clear
+	tmux new-session -s "$process" -d 'sudo bash run.sh '$process
+	exit
+fi
