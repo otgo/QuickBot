470e6ec6c1376450a537428520c520b14f419045
Harry <jarriztg@users.noreply.github.com>
2016-08-15 23:40:30 -0500

    back in backxd

--- run.sh
+++ run.sh
@@ -1,25 +1,9 @@
 session=QuickBot
 session2=ScriptGban
-process=process-115-no_exit
-
-do_secure_process() {
-	secure="process-115"
-	while true
-	do
-		lua bot.lua
-		echo -e 'crash'
-	for i in 1
-	do
-		echo "$i..."
-	done
-		echo "terminado"
-	done
-}
 
 if [ "$1" = "" ]; then
 	clear
 	tmux kill-session -t $session
-	tmux kill-session -t $process
 	clear
 	tmux new-session -s "$session" -d 'lua bot.lua'
 	tmux attach -t "$session"
@@ -39,20 +23,7 @@ fi
 
 if [ "$1" = "kill" ]; then
 	clear
-	tmux kill-session -t $process
 	tmux kill-session -t $session
 	tmux kill-session -t $session2
 fi
 
-if [ "$1" == "$process" ]; then
-	do_secure_process
-fi
-
-if [ "$1" == "secure" ]; then
-	clear
-	tmux kill-session -t $session
-	tmux kill-session -t $process
-	clear
-	tmux new-session -s "$process" -d 'sudo bash run.sh '$process
-	exit
-fi
