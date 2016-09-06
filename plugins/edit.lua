33baf60280a9466559f886baa4df2eaf627cfc8f
xxdamage <xxxdamagee@gmail.com>
2016-09-07 01:01:55 +0200

    Update edit.lua

--- plugins/edit.lua
+++ plugins/edit.lua
@@ -1,6 +1,6 @@
 local action = function(msg, matches, blocks, ln)
  if msg.reply then
-  if is_mod(msg) then
+  if is_owners(msg) then
    api.editMessageText(msg.chat.id, msg.reply.message_id, matches[1])
 else 
 	api.sendReply(msg, '🚫 *no tienes poderes para hacer eso :)*', true)
