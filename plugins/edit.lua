242f1a314581d6786b0fa5b6a311ae7206724b91
xxdamage <xxxdamagee@gmail.com>
2016-09-06 21:19:48 +0200

    Created edit.lua
    you can edit bot messages by reply

---
+++ plugins/edit.lua
@@ -0,0 +1,19 @@
+local action = function(msg, matches, blocks, ln)
+ if msg.reply then
+  if is_mod(msg) then
+   api.editMessageText(msg.chat.id, msg.reply.message_id, matches[1])
+else 
+	api.sendReply(msg, '🚫 *no tienes poderes para hacer eso :)*', true)
+  end
+ end
+end
+    
+return {
+ action = action,
+ triggers = {
+  '^[!#/]edit (.*)',
+ }
+}
+
+
+--by @xxdamage
